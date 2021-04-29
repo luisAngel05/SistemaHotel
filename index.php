0bcb0579f40f03d99fe750f43a827e15a395622c
luisAngel05 <luislalh05@gmail.com>
2021-04-28 19:56:04 -0500

    index

---
+++ index.php
@@ -0,0 +1,42 @@
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>sistema de informacion</title>
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/formato.css">
+                                
+</head>
+<body>
+<section >
+<fieldset >Bienvenido</fieldset>
+         <form id="elementos">
+         	       <h3>Usuario:</h3> <input type="text" id="User" placeholder="ID de Usuario" maxlength="10" ><br>
+         	  <h3> Contraseña: </h3><input type="password"id="pass" placeholder="Contraseña" maxlength="10" >
+
+         </form>
+</section>
+<section>
+	     <form id="iniciarSesion">
+	     	<input type="submit" value="Iniciar Sesion">
+	     </form>
+</section>
+<section>
+	     <form id="admin">
+	     	<input type="submit" value="Soy administrador">
+	     </form>
+</section>
+<br>
+
+<div id="CCN">
+	<a href="">Crear una cuenta nueva</a>
+</div>
+
+
+
+
+
+
+
+
+</body>
+</html>
